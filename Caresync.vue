c5acc0ab0a5436f0014781485ff7cae837bc96ac
Miguel10012 <134004486+Miguel10012@users.noreply.github.com>
2023-05-21 15:28:03 +0300

    Create Caresync.vue

---
+++ Caresync.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <header>
+      <!-- Header component -->
+      <Header />
+    </header>
+    <main>
+      <!-- Router view for rendering different pages -->
+      <router-view />
+    </main>
+    <footer>
+      <!-- Footer component -->
+      <Footer />
+    </footer>
+  </div>
+</template>
+
+<script>
+// Import necessary components
+import Header from './components/layout/Header.vue';
+import Footer from './components/layout/Footer.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
+
+<style>
+/* Global styles for the app */
+</style>
